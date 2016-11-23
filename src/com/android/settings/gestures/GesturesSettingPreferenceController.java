da1b83d8ab3ace81859709228b885ef51952502e
Chris Crump <chris@cypheros.co>
2016-11-23 13:45:00 +0200

    Settings: Forward Port Volume Long Press Skip Tracks [2/3]
    Bring Forward Volume Rocker Track Change
    
    * Thanks Sahul Krishan for the great animation
    * ritujb: Fix default value mismatch with framework in playback control
    
    Change-Id: Ifa3d0987a0d54afd1f56e1d4a7488f428338c757
    Signed-off-by: Mesquita <mesquita@aospa.co>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- src/com/android/settings/gestures/GesturesSettingPreferenceController.java
+++ src/com/android/settings/gestures/GesturesSettingPreferenceController.java
@@ -27,6 +27,7 @@ import com.android.settings.overlay.FeatureFactory;
 import com.android.settingslib.core.AbstractPreferenceController;
 
 import com.pixeldust.settings.gestures.powermenu.GlobalPowerMenuPreferenceController;
+import com.pixeldust.settings.gestures.volumerocker.VolumeButtonMusicControlPreferenceController;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -80,6 +81,7 @@ public class GesturesSettingPreferenceController extends BasePreferenceControlle
                 .setConfig(ambientDisplayConfiguration));
         controllers.add(new PreventRingingParentPreferenceController(context, FAKE_PREF_KEY));
         controllers.add(new GlobalPowerMenuPreferenceController(context, FAKE_PREF_KEY));
+        controllers.add(new VolumeButtonMusicControlPreferenceController(context, FAKE_PREF_KEY));
         return controllers;
     }
 }
