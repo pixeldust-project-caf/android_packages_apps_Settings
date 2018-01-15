f0128c80e20d4dfd1378736af8ca8d365fbae049
Bruno Martins <bgcngm@gmail.com>
2018-01-15 08:44:46 +0000

    Settings: Add switch for linked ring and media notification volumes
    [mickaelmendes50]
     - Use AOSP Setting Pref. Controller for linker Switch
       instead of Custom Setting Switch Preference.
     - Update Ring Volume Seekbar string.
     - Update Notification Volume Seekbar icon and move it
       above Alarm Volume Seekbar
    
    Author: Dan Pasanen <dan.pasanen@gmail.com>
    Date:   Wed Oct 30 09:22:30 2019 -0500
    
        VolumePanel: fix showing notification volume slider when unlinked
    
        * volume_link_notification lives in Settings.Secure not LineageSettings.Secure
        * Set max panel slices to 6. We could have up to 5 volumes to control and
          MEDIA_OUTPUT_INDICATOR_SLICE_URI counts as one slice as well.
    
        Change-Id: I0a4c7db1c92e778fba669931259cd52164b1df65
    
    Settings: Use correct icon for ring volume
    
    Author: Han Wang <416810799@qq.com>
    Date:   Tue Aug 4 13:33:45 2020 +0200
    
        Settings: Fix icon size for ring volume
    
        Change-Id: Idc62abbaea8d4d4ccb72f4a9f395ce0f861dc705
    
        Change-Id: If145262562c1409cd8a6cb79ba46b6646c75d1a8
        Signed-off-by: PMS22 <prathams99@rediff.com>
    
    Change-Id: I900af398f566a5415c44fe883cc214985f4e9cd4
    Co-authored-by: Mesquita <mesquita@aospa.co>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- src/com/android/settings/notification/SoundSettings.java
+++ src/com/android/settings/notification/SoundSettings.java
@@ -269,6 +269,8 @@ public class SoundSettings extends DashboardFragment implements OnActivityResult
                 new EmergencyTonePreferenceController(context, fragment, lifecycle);
         final VibrateIconPreferenceController vibrateIconPreferenceController =
                 new VibrateIconPreferenceController(context, fragment, lifecycle);
+        final LinkedVolumesPreferenceController linkedVolumesPreferenceController =
+                new LinkedVolumesPreferenceController(context, fragment, lifecycle);
 
         controllers.add(dialPadTonePreferenceController);
         controllers.add(screenLockSoundPreferenceController);
@@ -279,6 +281,7 @@ public class SoundSettings extends DashboardFragment implements OnActivityResult
         controllers.add(dockAudioMediaPreferenceController);
         controllers.add(bootSoundPreferenceController);
         controllers.add(emergencyTonePreferenceController);
+        controllers.add(linkedVolumesPreferenceController);
         controllers.add(new PreferenceCategoryController(context,
                 "other_sounds_and_vibrations_category").setChildren(
                 Arrays.asList(dialPadTonePreferenceController,
@@ -289,7 +292,8 @@ public class SoundSettings extends DashboardFragment implements OnActivityResult
                         vibrateIconPreferenceController,
                         dockAudioMediaPreferenceController,
                         bootSoundPreferenceController,
-                        emergencyTonePreferenceController)));
+                        emergencyTonePreferenceController,
+                        linkedVolumesPreferenceController)));
 
         return controllers;
     }
