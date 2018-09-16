98c54f7da1876c18b439144306daa4bcce53159a
bigrushdog <randall.rushing@gmail.com>
2018-09-15 17:20:30 -0700

    Initial checkin on Pie for SmartNav navigation suite [2/4]
    Interesting changes for bring up:
    
    The DUI project is now called SmartNav in a effort to bring
    broader generalization to the project
    
    The SmartNav settings have been moved to a separate project.
    This will allow for MUCH EASIER adaptation for projects in which
    only minimal touching of Settings and project specific settings
    is required.
    
    Elimination of the previously required utils jar shared library!
    We moved all the utils classes to fwb. This eliminated the need
    to hax up bp files and touch makefiles all over to use utils
    methods, as well as simplifies project integration.
    
    Wrapping stock navigation implementation in the Navigator
    abstraction is becoming increasingly difficult as more
    "things" interact with stock navigation buttons. We handle this
    situation with button null checks as well as "isStockNav()" checks.
    
    As SystemUI continues to compartmentalize, required notification media
    methods in StatusBar moved to a separate class. Adaptation for Pulse
    required the introduction of a new callback from NotificationMediaManager
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -169,4 +169,8 @@ public class Settings extends SettingsActivity {
     public static class SMQQtiFeedbackActivity extends SettingsActivity { /* empty */ }
 
     public static class PixelDustSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class NavigationSettingsActivity extends SettingsActivity {}
+    public static class SmartbarSettingsActivity extends SettingsActivity {}
+    public static class FlingSettingsActivity extends SettingsActivity {}
+    public static class PulseSettingsActivity extends SettingsActivity {}
 }
