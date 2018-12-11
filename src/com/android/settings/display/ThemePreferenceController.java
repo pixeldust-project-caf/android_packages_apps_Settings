c4a35034f7a91c6d3375da60823b96d2fec652be
Kshitij Gupta <kshitijgm@gmail.com>
2018-12-11 19:25:29 +0530

    display: Disable stock theme picker
    Change-Id: I6e35c5692e268618a17f4edde28d78bea4cf16c3
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- src/com/android/settings/display/ThemePreferenceController.java
+++ src/com/android/settings/display/ThemePreferenceController.java
@@ -144,7 +144,7 @@ public class ThemePreferenceController extends AbstractPreferenceController impl
     public boolean isAvailable() {
         if (mOverlayService == null) return false;
         String[] themes = getAvailableThemes();
-        return themes != null && themes.length > 1;
+        return themes != null && themes.length > 1 && false;
     }
 
 
