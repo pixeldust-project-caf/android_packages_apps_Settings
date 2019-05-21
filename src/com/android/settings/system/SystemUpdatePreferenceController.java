PixelExperience/packages_apps_Settings

SystemUpdatePreferenceController: Remove gsi check
We don't support that anymore

Change-Id: I3bd06f9cb7d0601488ca0464d75264b85b0ff44a

src/com/android/settings/system/SystemUpdatePreferenceController.java
@@ -45,7 +45,7 @@ public class SystemUpdatePreferenceController extends BasePreferenceController {
     public int getAvailabilityStatus() {
         String buildtype = SystemProperties.get(OTA_BUILD_TYPE_PROP,"unofficial");
         String deviceName = SystemProperties.get(DEVICE_NAME,"");
-        if (deviceName.startsWith("phhgsi_") || !mUm.isAdminUser() || !buildtype.equalsIgnoreCase("official")){
+        if (!mUm.isAdminUser() || !buildtype.equalsIgnoreCase("official")){
             return UNSUPPORTED_ON_DEVICE;
         }
         try {

pixeldust-project-caf/android_packages_apps_Settings

Signed-off-by: spezi77 <spezi7713@gmx.net>

src/com/android/settings/system/SystemUpdatePreferenceController.java
@@ -45,7 +45,7 @@ public class SystemUpdatePreferenceController extends BasePreferenceController {
     public int getAvailabilityStatus() {
         String buildtype = SystemProperties.get(OTA_BUILD_TYPE_PROP,"unofficial");
         String deviceName = SystemProperties.get(DEVICE_NAME,"");
-        if (deviceName.startsWith("phhgsi_") || !mUm.isAdminUser() || !buildtype.equalsIgnoreCase("release")){
+        if (!mUm.isAdminUser() || !buildtype.equalsIgnoreCase("release")){
             return UNSUPPORTED_ON_DEVICE;
         }
         try {