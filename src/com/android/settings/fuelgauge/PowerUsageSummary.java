7a470cf9665cffe36f653e8791b0b92364e11307
spezi77 <spezi7713@gmx.net>
2019-07-16 20:47:33 +0200

    Add an overlay settings field for Smart Charging. Allows a device maintainer to show/hide the Smart Charging pref in Power Settings. As this feature ain't going to work for all devices hide the preferences by default.
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Change-Id: I0dcbdab57e5cef2e599054f570137078a751d4ac

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -91,6 +91,8 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
     private static final String KEY_TIME_SINCE_LAST_FULL_CHARGE = "last_full_charge";
     private static final String KEY_BATTERY_SAVER_SUMMARY = "battery_saver_summary";
 
+    private static final String KEY_SMART_CHARGING = "smart_charging_key";
+
     @VisibleForTesting
     static final int BATTERY_INFO_LOADER = 1;
     @VisibleForTesting
@@ -251,6 +253,12 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
         restartBatteryInfoLoader();
         mBatteryTipPreferenceController.restoreInstanceState(icicle);
         updateBatteryTipFlag(icicle);
+
+        // Check availability of Smart Charging
+        Preference mSmartChargingPref = (Preference) findPreference(KEY_SMART_CHARGING);
+        if (!getResources().getBoolean(R.bool.config_supportSmartCharging)) {
+            getPreferenceScreen().removePreference(mSmartChargingPref);
+        }
     }
 
     @Override
