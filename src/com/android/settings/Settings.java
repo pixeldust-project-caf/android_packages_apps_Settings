5331e39efbd49df668620d871513dffd33e2e6a7
spezi77 <spezi7713@gmx.net>
2019-10-01 21:30:20 +0200

    Settings: Add quick settings category for PDSettings.
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -180,4 +180,5 @@ public class Settings extends SettingsActivity {
     public static class SMQQtiFeedbackActivity extends SettingsActivity { /* empty */ }
 
     public static class PixelDustSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class QuickSettingsActivity extends SettingsActivity { /* empty */ }
 }
--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -149,6 +149,7 @@ import com.android.settings.wifi.savedaccesspoints.SavedAccessPointsWifiSettings
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
 import com.pixeldust.settings.PixelDustSettings;
+import com.pixeldust.settings.fragments.QuickSettings;
 
 public class SettingsGateway {
 
@@ -287,6 +288,7 @@ public class SettingsGateway {
             MobileNetworkListFragment.class.getName(),
             GlobalActionsPanelSettings.class.getName(),
             PixelDustSettings.class.getName(),
+            QuickSettings.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -327,5 +329,6 @@ public class SettingsGateway {
             Settings.ModuleLicensesActivity.class.getName(),
             UserBackupSettingsActivity.class.getName(),
             Settings.PixelDustSettingsActivity.class.getName(),
+            Settings.QuickSettingsActivity.class.getName(),
     };
 }
