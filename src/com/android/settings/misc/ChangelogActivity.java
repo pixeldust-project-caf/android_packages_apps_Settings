482496fc830555e6d6e22daeb6471f55b7b5af32
spezi77 <spezi7713@gmx.net>
2022-12-18 13:14:23 +0100

    ChangelogActivity: Update for tm-qpr1.
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- src/com/android/settings/misc/ChangelogActivity.java
+++ src/com/android/settings/misc/ChangelogActivity.java
@@ -21,7 +21,7 @@ package com.android.settings.misc;
 import android.os.Bundle;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import com.android.settingslib.collapsingtoolbar.R;
+import com.android.settingslib.widget.R;
 
 public class ChangelogActivity extends CollapsingToolbarBaseActivity {
     @Override
