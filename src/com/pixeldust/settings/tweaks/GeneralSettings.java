a43a0d94892a503e338f43226bca945d6eae5f8b
spezi77 <spezi7713@gmx.net>
2023-01-19 22:37:38 +0100

    General tweaks: User toggle for unlimited photos storage.
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- src/com/pixeldust/settings/tweaks/GeneralSettings.java
+++ src/com/pixeldust/settings/tweaks/GeneralSettings.java
@@ -7,6 +7,13 @@
 package com.pixeldust.settings.tweaks;
 
 import android.os.Bundle;
+import android.os.SystemProperties;
+import android.widget.Toast;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.SwitchPreference;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.R;
@@ -15,7 +22,12 @@ import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 
 @SearchIndexable
-public class GeneralSettings extends SettingsPreferenceFragment {
+public class GeneralSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+
+    private static final String KEY_SPOOF = "use_photos_spoof";
+    private static final String SYS_SPOOF = "persist.sys.pixelprops.gphotos";
+
+    private SwitchPreference mSpoof;
 
     @Override
     public int getMetricsCategory() {
@@ -26,6 +38,22 @@ public class GeneralSettings extends SettingsPreferenceFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.general_settings);
+
+        mSpoof = (SwitchPreference) findPreference(KEY_SPOOF);
+        mSpoof.setChecked(SystemProperties.getBoolean(SYS_SPOOF, false));
+        mSpoof.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if (preference == mSpoof) {
+            String value = ((Boolean) objValue) ? "true" : "false";
+            SystemProperties.set(SYS_SPOOF, value);
+            Toast.makeText(getActivity(),
+                    (R.string.photos_spoof_toast), Toast.LENGTH_LONG).show();
+            return true;
+        }
+        return false;
     }
 
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
